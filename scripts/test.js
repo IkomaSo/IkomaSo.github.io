4df824b51f56e1c32af016be576549b25f82c614
Ikoma_So <hkrs@outlook.jp>
2018-01-16 22:53:52 +0900

    beta

--- scripts/test.js
+++ scripts/test.js
@@ -6,7 +6,7 @@ for (var i = 0; i < 3; i++) {
 
 function csvToArray(path) {
     var req = new XMLHttpRequest(); // HTTPでファイルを読み込むためのXMLHttpRrequestオブジェクトを生成
-    req.open("get", path, true); // アクセスするファイルを指定
+    req.open("get", path, false); // アクセスするファイルを指定
     req.send(null); // HTTPリクエストの発行
     text.push(req.responseText);
 }
@@ -24,6 +24,7 @@ csvToArray("./vocabularys/sample.csv");
 arrayToData();
 console.log(text[0][0]);
 console.log(data[0][0][0]);
+console.log(data[0][2][1]);
 var Hello = function(button) {
     anime({
         targets: 'html, body',
