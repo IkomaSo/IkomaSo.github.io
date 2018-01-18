c44db25c0c60a16f1f2e25b68dbb46b41b3b825a
Ikoma_So <hkrs@outlook.jp>
2018-01-18 19:03:49 +0900

    beta

--- scripts/test.js
+++ scripts/test.js
@@ -2,7 +2,7 @@ var cnt = 0;
 var elem = document.getElementById("range");
 console.log(elem);
 var rangeValue = function() {
-    var newValue = elem.val();
+    var newValue = elem.value;
     var target = document.querySelector('.value');
     target.innerHTML = newValue;
 }
