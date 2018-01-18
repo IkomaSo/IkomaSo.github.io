8ac8448e0b4622a962586193e25d30361a6e8ceb
Ikoma_So <hkrs@outlook.jp>
2018-01-18 19:06:24 +0900

    beta

--- scripts/change.js
+++ scripts/change.js
@@ -1,6 +1,15 @@
 AOS.init();
 Barba.Prefetch.init();
 Barba.Dispatcher.on('newPageReady', function() {
+    var elem = document.getElementById("range");
+    console.log(elem);
+    var rangeValue = function() {
+        var newValue = elem.value;
+        var target = document.querySelector('.value');
+        target.innerHTML = newValue;
+    }
+
+    elem.addEventListener("input", rangeValue);
     AOS.init();
 });
 
