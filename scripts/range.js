b9429af75c133155e2ec3611d430eae630065988
Ikoma_So <hkrs@outlook.jp>
2018-01-18 19:12:58 +0900

    beta

---
+++ scripts/range.js
@@ -0,0 +1,9 @@
+var elem = document.getElementById("range");
+console.log(elem);
+var rangeValue = function() {
+    var newValue = elem.value;
+    var target = document.querySelector('.value');
+    target.innerHTML = newValue;
+}
+
+elem.addEventListener("input", rangeValue);
