ebf9d99dd95536bb645d29315c73c7906fdd8f39
Ikoma_So <hkrs@outlook.jp>
2018-01-19 23:02:51 +0900

    beta

--- scripts/test.js
+++ scripts/test.js
@@ -65,7 +65,7 @@ function select(choice) {
     }
     if (random9 == choice) {
         cnt++;
-        console.log(data[0][1][0]);
+        console.log(data[random1][random2][0]);
         document.getElementById("number").innerHTML = cnt + " / " + sum + " 問";
     } else {
         alert("不正解\n答え：" + ans);
